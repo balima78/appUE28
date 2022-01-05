61e7a1f28de5c068506eb4de324ca3c7a1ccd373
Bruno Lima <balima78@gmail.com>
2022-01-05 21:32:29 +0000

    added link to TxOR

--- update.R
+++ update.R
@@ -13,7 +13,7 @@ rsconnect::setAccountInfo(name='bioestatisticas',
                           secret='4DGi2kzm/a5i6ww9XiruPwzkNk0FVjTWNaPfV2Jm')
 
 library(rsconnect)
-rsconnect::deployApp("D:/CHN/OpenData/appUE28")
+rsconnect::deployApp("C:/Users/balim/Documents/4.APPs/appUE28")
 
 y
 
